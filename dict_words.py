0ad95289c8aa266e368661cc7c2f7749d0de3fcc
Perzival1312 <jj.mongoose11@hotmail.com>
2019-01-28 12:21:24 -0800

    [feature] word finish stretch challenge

--- dict_words.py
+++ dict_words.py
@@ -4,6 +4,14 @@ words_list = f.readlines()
 f.close()
 word_count = sys.argv[1:]
 final_words = []
-for i in range(int(word_count[0])):
-    final_words.append(random.choice(words_list))
+try:
+    int(word_count)
+    for _ in range(int(word_count[0])):
+        final_words.append(random.choice(words_list)) 
+except:
+    word_fragment = word_count[0]
+    for words in words_list:
+        temp_word = words[:len(word_fragment)]
+        if temp_word == word_fragment:
+            final_words.append(words)
 print("".join(final_words))
