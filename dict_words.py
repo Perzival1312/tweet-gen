37885785bf520dfe11a2032e07dfb11a0dfaad6f
Perzival1312 <jj.mongoose11@hotmail.com>
2019-01-28 10:52:00 -0800

    v1 dict words

--- dict_words.py
+++ dict_words.py
@@ -2,3 +2,8 @@ import random, sys
 f = open('/usr/share/dict/words', 'r')
 words_list = f.readlines()
 f.close()
+word_count = sys.argv[1:]
+final_words = []
+for i in range(int(word_count[0])):
+    final_words.append(random.choice(words_list))
+print("".join(final_words))
