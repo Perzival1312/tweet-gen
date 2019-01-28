589599efa7349f88e01722a5a9675fd5cc3e956c
Perzival1312 <jj.mongoose11@hotmail.com>
2019-01-28 11:57:10 -0800

    inplace shuffle implemented

--- rearrange.py
+++ rearrange.py
@@ -1,8 +1,21 @@
 import random, sys
 str_input = sys.argv[1:]
-shuffled = []
-for i in range(len(str_input)):
+# shuffled = []
+# for i in range(len(str_input)):
+#     choice = random.choice(str_input)
+#     shuffled.append(choice)
+#     str_input.remove(choice)
+# print(shuffled)
+
+length = len(str_input)
+for index, value in enumerate(str_input):
     choice = random.choice(str_input)
-    shuffled.append(choice)
-    str_input.remove(choice)
-print(shuffled)
+    choice_index = str_input.index(choice)
+    temp = value
+    str_input[index] = choice
+    str_input[choice_index] = temp
+print(str_input)
+    
+
+
+# random.randrange(len(words_list))[:-1]
