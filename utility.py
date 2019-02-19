964b0084631abd941b07953252eb583dbcde3bf2
Perzival1312 <jj.mongoose11@hotmail.com>
2019-02-19 11:49:47 -0800

    added package for overused functions

---
+++ utility.py
@@ -0,0 +1,29 @@
+import string
+
+string.punctuation += "”“’‘—"
+string.punctuation += string.digits
+
+def cleanse(text):
+    table = str.maketrans({key: None for key in string.punctuation})
+    words_list = []
+    for strings in text:
+        words_list += strings.split()
+    for word in range(len(words_list)):
+        words_list[word] = words_list[word].translate(table).lower()
+    return words_list
+
+def read(source):
+    texts_list = []
+    f = open(source, 'r')
+    texts_list = f.readlines()
+    f.close()
+    try:
+        for text in texts_list:
+            text = text.strip()
+            g = open(text, 'r')
+            word_list += g.readlines()
+            g.close()
+    except:
+        print(repr(text), "not a file")
+        word_list = texts_list
+    return word_list
