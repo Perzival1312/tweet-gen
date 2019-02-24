b90f11b1ebc61db12166cd7f59d2a8ebae05b65f
Perzival1312 <jj.mongoose11@hotmail.com>
2019-02-24 15:17:58 -0800

    made vocab game

---
+++ input_validation.py
@@ -0,0 +1,87 @@
+# TODO original.replace(' ','').isalpha()
+def single_letter(input_string):
+    valid_input = False
+    while not valid_input:
+        try:
+            testing_input = input(input_string)
+            if not testing_input.isalpha():
+                print("Only letters, please try again!")
+            elif len(testing_input)!=1:
+                print("Must be a singular letter, please try again!")
+            else:
+                valid_input = True
+                return testing_input
+        except EOFError:
+            print("Not a letter! Try again.")
+            continue
+
+def single_number(input_string):
+    valid_input = False
+    while not valid_input:
+        try:
+            testing_input = input(input_string)
+            if not testing_input.isnumeric():
+                print("Only numbers, please try again!")
+            elif len(testing_input)!=1:
+                print("Must be a singular number, please try again!")
+            else:
+                valid_input = True
+                return testing_input
+        except EOFError:
+            print("Not a number! Try again.")
+            continue
+
+
+def multi_letter(input_string, length=32):
+    valid_input = False
+    while not valid_input:
+        try:
+            testing_input = input(input_string)
+            if not testing_input.replace(' ','').isalpha():
+                print("That is not a letter, please try again!")
+            elif len(testing_input) >= length:
+                print("Must be less than " + str(length) + " characters long, please try again!")
+            else:
+                valid_input = True
+                return testing_input
+        except EOFError:
+            print("Not a letter! Try again.")
+            continue
+
+def multi_number(input_string, length=32):
+    valid_input = False
+    while not valid_input:
+        try:
+            testing_input = input(input_string)
+            if not testing_input.isnumeric():
+                print("That is not a number, please try again!")
+            elif len(testing_input) >= length:
+                print("Must be less than " + str(length) + " characters long, please try again!")
+            else:
+                valid_input = True
+                return testing_input
+        except EOFError:
+            print("Not a number! Try again.")
+            continue
+
+# TODO get this module working!!!
+def continuation(input_string):
+    valid_input = False
+    while not valid_input:
+        try:
+            testing_input = input(input_string)
+            if not testing_input.isalpha():
+                print("That is not a letter, please try again!")
+            elif len(testing_input)!=1:
+                print("Must be a singular letter, please try again!")
+            elif testing_input.lower() != "n" and testing_input.lower() != "y":
+                print("Must be Y or N")
+            else:
+                valid_input = True
+        except EOFError:
+            print("Not a letter! Try again.")
+            continue
+    if testing_input.lower() == "y":
+        return True
+    else:
+        return False
---
+++ vocab_game.py
@@ -0,0 +1,27 @@
+import random, sys, input_validation, os
+from PyDictionary import PyDictionary
+f = open('/usr/share/dict/words', 'r')
+words_list = f.readlines()
+f.close()
+cont = True
+dictionary = PyDictionary()
+
+while cont:
+    usable = True
+    while usable:
+        word = random.choice(words_list).strip()
+        sys.stdout = open(os.devnull, "w")
+        definition = dictionary.meaning(word)
+        sys.stdout = sys.__stdout__
+        if type(definition) == dict:
+            print(dictionary.meaning(word))
+            usable = False
+        else:
+            usable = True
+
+    guess = input("what is the word?\n")
+    if guess == word:
+        print("congratulations you are correct!")
+    else:
+        print("that is incorrect, it was {}".format(word))
+    cont = input_validation.continuation("Play again?(y/n)")
