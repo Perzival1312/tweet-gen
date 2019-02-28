56872dd257130136c3bf36f1d457a88ab9032305
Perzival1312 <jj.mongoose11@hotmail.com>
2019-02-27 18:45:49 -0800

    fixing heroku

--- config_module.py
+++ config_module.py
@@ -1,5 +1,5 @@
 import os
-from boto.s3.connection import S3Connection
+# from boto.s3.connection import S3Connection
 
 class Config(object):
     DEBUG = False
@@ -7,7 +7,7 @@ class Config(object):
     DATABASE_URI = 'localhost:27017'
 
 class ProductionConfig(Config):
-    DATABASE_URI = S3Connection(os.environ['MONGODB_URI'])
+    DATABASE_URI = os.environ['MONGODB_URI']
 
 class DevelopmentConfig(Config):
     DEBUG = True
