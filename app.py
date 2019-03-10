9156e924b56293f33420da425089a1cd813700ac
Perzival1312 <jj.mongoose11@hotmail.com>
2019-03-10 09:21:25 -0400

    linked source to saved quote

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ import mongoengine
 from pymongo import MongoClient 
 from mongoengine import (Document, connect, StringField)
 from flask_mongoengine import QuerySet
-import utility, os
+import os, json
+import utility
 from nth_order_markov_for_web import Dictogram
 import config_module
 
@@ -24,10 +25,14 @@ else:
 class sources(Document):
     title = StringField(required=True, max_length=200)
     content = StringField(required=True)
+    first_order = StringField(required=True)
+    second_order = StringField(required=True)
+    third_order = StringField(required=True)
+    fourth_order = StringField(required=True)
 
 class sentences(Document):
     content = StringField(required=True)
-    # source = StringField(required=True)
+    source = StringField(required=True)
 
 @app.route('/')
 def home():
@@ -43,13 +48,13 @@ def home():
     # return redirect('/sentence', code=302)
     histogram.get_sentence()
     sentence = histogram.print_sentence()
-    return render_template('index.html', test = sentence)
+    return render_template('index.html', test = sentence, sentence_source = source['title'])
 
 @app.route('/', methods=['POST'])
 def save():
     data = request.form
-    possible_sent = sentences(content= data['sentence'])#, source = source['title'])
-    print(possible_sent.content)
+    possible_sent = sentences(content= data['sentence'], source = data['source'])
+    print(possible_sent.content, possible_sent.source)
     possible_sent.save()
     return redirect('/', code=302)
 
@@ -69,16 +74,28 @@ def show():
     return render_template('saved.html', sentences = saved)
 
 
-# @app.route('/load_sources')
-# def load_sources():
-#     texts_list, word_list = [], []
-#     f = open('texts.txt', 'r')
-#     texts_list = f.readlines()
-#     f.close()
-#     for text in texts_list:
-#         g = open(text.strip(), 'r')
-#         word_list = g.readlines()
-#         g.close()
-#         new_text = sources(title=text, content=" ".join(word_list))
-#         new_text.save()
-#     return redirect('/', code=302)
+@app.route('/load_sources')
+def load_sources():
+    sources.drop_collection()
+    texts_list, word_list = [], []
+    f = open('texts.txt', 'r')
+    texts_list = f.readlines()
+    f.close()
+    for text in texts_list:
+        g = open(text.strip(), 'r')
+        word_list = g.readlines()
+        g.close()
+        words = utility.cleanse(word_list)
+        histogram_first = Dictogram(words, 1)
+        histogram_first.count_to_possibility()
+        histogram_second = Dictogram(words, 2)
+        histogram_second.count_to_possibility()
+        histogram_third = Dictogram(words, 3)
+        histogram_third.count_to_possibility()
+        histogram_fourth = Dictogram(words, 4)
+        histogram_fourth.count_to_possibility()
+        new_text = sources(title=text, content=" ".join(word_list), 
+                            first_order=json.dumps(histogram_first), second_order=json.dumps(histogram_second), 
+                            third_order=json.dumps(histogram_third), fourth_order=json.dumps(histogram_fourth))
+        new_text.save()
+    return redirect('/', code=302)
