34c848ad4212027c0cd5fad8ef5addbf65af2183
Perzival1312 <jj.mongoose11@hotmail.com>
2019-03-17 17:25:44 -0500

    supported previously unsupport necassary strings

--- static/indexScripts.js
+++ static/indexScripts.js
@@ -1,4 +1,7 @@
-let sourceName = document.getElementById('source-for-selector').defaultValue.slice(8, -5);
+let sourceName = document.getElementById('source-for-selector').defaultValue;
+let beginSliceInd = sourceName.indexOf('/');
+let endSliceIndex = sourceName.indexOf('.');
+sourceName  = sourceName.slice(beginSliceInd+1, endSliceIndex);
 const mySelect = document.getElementById('source-selector');
 
 for(let i, j = 0; i = mySelect.options[j]; j++) {
