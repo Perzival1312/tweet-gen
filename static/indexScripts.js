af553750c5e207ad03e808440ab00d8ac6b286b2
Perzival1312 <jj.mongoose11@hotmail.com>
2019-03-17 18:06:26 -0500

    better titles on saved

--- static/indexScripts.js
+++ static/indexScripts.js
@@ -3,10 +3,16 @@ let beginSliceInd = sourceName.indexOf('/');
 let endSliceIndex = sourceName.indexOf('.');
 sourceName  = sourceName.slice(beginSliceInd+1, endSliceIndex);
 const mySelect = document.getElementById('source-selector');
-
+// sets correct value to dropdown menu
 for(let i, j = 0; i = mySelect.options[j]; j++) {
     if(i.value == sourceName) {
         mySelect.selectedIndex = j;
         break;
     }
+}
+// gets the conversion table for titles so that they are "readable"
+let conversionObj = {}
+for(let i=0; i<mySelect.options.length; i++){
+    conversionObj = Object.assign(conversionObj, {[mySelect.options[i].value]: mySelect.options[i].text})
+    localStorage[mySelect.options[i].value] =  mySelect.options[i].text
 }
--- static/savedScripts.js
+++ static/savedScripts.js
@@ -4,7 +4,7 @@ arrs = arr.slice(1, arr.length-1)
 splitArr = arrs.split(', ')
 retStr = ""
 for(let i=0; i<splitArr.length; i+=2) {
-    splitArr[i] = '<h1>'+splitArr[i]+'\t-'+splitArr[i+1].slice(1, -1)+'<hr /></h1>'
+    splitArr[i] = '<h1>'+splitArr[i]+'\t-'+localStorage[splitArr[i+1].slice(1, -1)]+'<hr /></h1>'
     retStr += splitArr[i]
 }
 data.innerHTML = retStr
