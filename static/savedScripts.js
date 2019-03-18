d44e382adf11bae468c38e5e06ce22c721b44e41
Perzival1312 <jj.mongoose11@hotmail.com>
2019-03-18 16:21:38 -0700

    added spacing on source

--- static/savedScripts.js
+++ static/savedScripts.js
@@ -3,8 +3,9 @@ const arr = document.getElementById('sentence-array').innerText;
 arrs = arr.slice(1, arr.length-1)
 splitArr = arrs.split(', ')
 retStr = ""
+tabStr = "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp" //8 spaces
 for(let i=0; i<splitArr.length; i+=2) {
-    splitArr[i] = '<h1>'+splitArr[i]+'\t-'+localStorage[splitArr[i+1].slice(1, -1)]+'<hr /></h1>'
+    splitArr[i] = '<h1>'+splitArr[i]+'<br>'+tabStr+'-'+localStorage[splitArr[i+1].slice(1, -1)]+'<hr /></h1>'
     retStr += splitArr[i]
 }
 data.innerHTML = retStr
