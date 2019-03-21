73a51dcc8f8a281950205593d8b739fb5dd87fd5
Perzival1312 <jj.mongoose11@hotmail.com>
2019-03-20 23:07:12 -0700

    better cleansing

--- utility.py
+++ utility.py
@@ -1,4 +1,4 @@
-import string, os
+import string, os, re
 end_punt = ".!?"
 string.punctuation += "”“’‘—"
 string.punctuation += string.digits
@@ -9,13 +9,11 @@ def cleanse(text):
     words_list = []
     periods = str.maketrans({key: period_str for key in end_punt})
     other_punct = str.maketrans({key: None for key in string.punctuation})
-    # if(os.environ['SETTINGS'] == 'DevelopmentConfig'):
-    #     for sentences in text:
-    #         words_list += sentences.split()
-    # if(os.environ['SETTINGS'] == 'ProductionConfig'):
-    #     words_list = text.split()
-    for sentences in text:
-        words_list += sentences.split()
+
+    text = ' '.join(text)
+    text = re.sub(r'(.|\n)*(\*{2,3}[ ]?(START OF).*(\n)?.*\*{2,3})', "", text, count=1) 
+    text = re.sub(r'(\*{2,3}[ ]?(END OF).*(\n)?.*\*{2,3})(.|\n)*', "", text, count=1) 
+    words_list = text.split()
     
     for word in range(len(words_list)):
         if words_list[word] != 'I' or words_list[word] != 'i':
